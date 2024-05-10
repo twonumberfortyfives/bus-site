03c7e96edc4f5679ea6a3d25dd9fb288281ff9dd
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-10 14:06:12 +0300

    django drf practice views

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,33 @@
-from django.urls import path
-from station.views import bus_list, bus_detail
+from django.urls import path, include
+from rest_framework import routers
 
+from station.views import BusViewSet
 
+# simplifying urls
+
+router = routers.DefaultRouter()
+router.register("buses", BusViewSet)
+
+#urlpatterns = router.urls
+# _____________________________________________________________________________________________________________________
 urlpatterns = [
-    path('buses/', bus_list, name="bus-list"),
-    path('buses/<int:pk>/', bus_detail, name="bus-detail")
+    path("", include(router.urls))
 ]
 
+# _____________________________________________________________________________________________________________________
+# bus_list = BusViewSet.as_view(actions={"get": "list", "post": "create"})
+# bus_detail = BusViewSet.as_view(
+#     actions={
+#         "get": "retrieve",
+#         "put": "update",
+#         "patch": "partial_update",
+#         "delete": "destroy"
+#     }
+# )
+#
+# urlpatterns = [
+#     path('buses/', bus_list, name="bus-list"),
+#     path('buses/<int:pk>/', bus_detail, name="bus-detail")
+# ]
+#
 app_name = "station"
--- station/views.py
+++ station/views.py
@@ -1,38 +1,154 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics, mixins, viewsets
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
+# USING MANUAL VIEW FUNC TO MANIPULATE REQUESTS
+# ___________________________________________________________________
+# @api_view(["GET", "POST"])
+# def bus_list(request):
+#     if request.method == 'GET':
+#         buses = Bus.objects.all()
+#         serializer = BusSerializer(buses, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     elif request.method == "POST":
+#         serializer = BusSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# _____________________________________________________________________________
+#
+# SIMPLE WAY AND MORE CLEAN BY USING CLASS VIEWS
+#
+# class BusListView(APIView):
+#     def get(self, request) -> Response:
+#         buses = Bus.objects.all()
+#         serializer = BusSerializer(buses, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     def post(self, request) -> Response:
+#         serializer = BusSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# ___________________________________________________________________________________
+#
+#  USING GenericAPIView TO USE OTHER MIXINS FOR CRUD
+#
+# class BusListView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
 
-@api_view(["GET", "POST"])
-def bus_list(request):
-    if request.method == 'GET':
-        buses = Bus.objects.all()
-        serializer = BusSerializer(buses, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "POST":
-        serializer = BusSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# USING generics views LIBRARY WHAT CONTAINS SHORTEST WAY FOR IMPLEMENTATION CRUD
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def bus_detail(request, pk: int):
-    bus = get_object_or_404(Bus, pk=pk)
-    if request.method == 'GET':
-        serializer = BusSerializer(bus)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == "PUT":
-        serializer = BusSerializer(bus, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == "DELETE":
-        bus.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class BusListView(generics.ListCreateAPIView):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+#
+#
+# class BusDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+
+# _____________________________________________________________________________________________________________________
+# @api_view(["GET", "PUT", "DELETE"])
+# def bus_detail(request, pk: int):
+#     bus = get_object_or_404(Bus, pk=pk)
+#     if request.method == 'GET':
+#         serializer = BusSerializer(bus)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     elif request.method == "PUT":
+#         serializer = BusSerializer(bus, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == "DELETE":
+#         bus.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# _____________________________________________________________________________________________________________________
+# class BusDetailView(APIView):
+#     def get_object(self, pk: int) -> Bus:
+#         return Bus.objects.get(pk=pk)
+#
+#     def get(self, request, pk: int) -> Response:
+#         serializer = BusSerializer(self.get_object(pk=pk))
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#
+#     def put(self, request, pk: int) -> Response:
+#         serializer = BusSerializer(self.get_object(pk=pk),
+#                                    data=request.data)  # possible to implement partial parameter for PATCH method
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#     def delete(self, request, pk: int) -> Response:
+#         self.get_object(pk=pk).delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+# _____________________________________________________________________________________________________________________
+#
+# class BusDetailView(
+#     generics.GenericAPIView,
+#     mixins.RetrieveModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin,
+# ):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+#
+#     def get(self, request, *args, **kwargs) -> Bus:
+#         return self.retrieve(request, *args, **kwargs)
+#
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+# _____________________________________________________________________________________________________________________
+# CREATION VIEWS WITH viewsets AND CHANGING URLS IN THE APP (USE ACTIONS ATTRIBUTE )
+# ALSO IS POSSIBLE TO COMBINE TWO CLASSES IN ONE
+
+# class BusListView(
+#     viewsets.GenericViewSet,
+#     mixins.ListModelMixin,
+#     mixins.CreateModelMixin
+# ):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+#
+#
+# class BusDetailView(
+#     viewsets.GenericViewSet,
+#     mixins.RetrieveModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin
+# ):
+#     queryset = Bus.objects.all()
+#     serializer_class = BusSerializer
+# _____________________________________________________________________________________________________________________
+#
+# MERGED INTO ONE CLASS (LINKS ARE CHANGED
+# USED viewsets.ModelViewSet what contains all views
+
+class BusViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
